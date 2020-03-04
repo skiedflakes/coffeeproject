b2da0f4a7802779c14aaf3d57634dba4c16d2e4e
thechosenweak <thechosenweak@gmail.com>
2020-03-04 11:50:14 +0800

    updates

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,59 +2,70 @@ import * as React from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 
 export default class  LoginScreen extends React.Component{
-    render(){
-        return(
-        <View style={styles.Layout}>
-          <View style={styles.loginLayout}>
-          <Text style={{textAlign:"center", marginBottom: 10}}>Coffe Project</Text>
-          <TextInput style = {styles.input}
-                underlineColorAndroid = "transparent"
-                placeholder = "Email"
-                placeholderTextColor = "#9a73ef"
-                autoCapitalize = "none"
-                onChangeText = {(text) => this.userName = text}/>
-          <TextInput style = {styles.input}
-                underlineColorAndroid = "transparent"
-                placeholder = "Password"
-                placeholderTextColor = "#9a73ef"
-                autoCapitalize = "none"
-                onChangeText = {(text) => this.password = text}/>
-            <Button
-              title="Login"
-              onPress={() => this.loginFetch()}
-            />
-          </View>
-        </View>
-        );
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: [],
+      userName: '',
+      password: ''
     }
+  }
 
-    loginFetch(){
-      fetch('http://192.168.1.105/schednotes_2020/login_test.php', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: this.userName,
-          password: this.password
-        })
-      }).then((response) => response.json())
-            .then((responseJson) => {
-  
-              const first = responseJson.login_response[0]
-        
-              if(first.status == 'success'){
-                //this.props.navigation.navigate('Home')
-                Alert.alert('success !');
-              } else {
-                Alert.alert('failed !');
-              }
-  
-            }).catch((error) => {
-              console.error(error);
-            });
-      }
+  render(){
+      return(
+      <View style={styles.Layout}>
+        <View style={styles.loginLayout}>
+        <Text style={{textAlign:"center", marginBottom: 10}}>Coffe Project</Text>
+        <TextInput style = {styles.input}
+              underlineColorAndroid = "transparent"
+              placeholder = "Email"
+              placeholderTextColor = "#9a73ef"
+              autoCapitalize = "none"
+              onChangeText = {(text) => this.userName = text}/>
+        <TextInput style = {styles.input}
+              underlineColorAndroid = "transparent"
+              placeholder = "Password"
+              placeholderTextColor = "#9a73ef"
+              autoCapitalize = "none"
+              onChangeText = {(text) => this.password = text}/>
+          <Button
+            title="Login"
+            onPress={() => this.loginFetch()}
+          />
+        </View>
+      </View>
+      );
+  }
+
+  loginFetch(){
+    fetch('http://192.168.1.105/schednotes_2020/login_test.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.userName,
+        password: this.password
+      })
+    }).then((response) => response.json())
+          .then((responseJson) => {
+
+            const first = responseJson.login_response[0]
+      
+            if(first.status == 'success'){
+              //this.props.navigation.navigate('Home')
+              Alert.alert('success !');
+            } else {
+              Alert.alert('failed !');
+            }
+
+          }).catch((error) => {
+            console.error(error);
+          });
+    }
 }
 
 const styles = StyleSheet.create({
