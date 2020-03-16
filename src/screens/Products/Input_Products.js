f6674b40ee9e8c45fa0da9495684ca3fa23f392e
thechosenweak <thechosenweak@gmail.com>
2020-03-16 16:50:00 +0800

    update

--- src/screens/Products/Input_Products.js
+++ src/screens/Products/Input_Products.js
@@ -1,24 +1,144 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { Text, View, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
 
 export default class Input_Products extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      filepath: {
+        data: '',
+        uri: ''
+      },
+      fileData: '',
+      fileUri: '',
+      productName: ''
+    }
   }
   
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.logo}
-          source={{
-uri:'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
-          }}
-        />
-        
+
+        <TouchableOpacity onPress={this.chooseImage}>
+          <Text>Choose File</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.launchCamera}>
+          <Text>Directly Launch Camera</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.launchImageLibrary}>
+          <Text>Directly Launch Image Library</Text>
+        </TouchableOpacity>
+
+        <View style={{marginTop: 10}}>
+        <TextInput
+              placeholder = "Enter name here..."
+              placeholderTextColor = "#9a73ef"
+              autoCapitalize = "none"
+              onChangeText = {(text) => this.productName = text}/>
+        </View>
+
+
+
       </View>
     );
+  }
+
+  chooseImage = () => {
+    let options = {
+      title: 'Select Image',
+      customButtons: [
+        { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+        // alert(JSON.stringify(response));s
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+  }
+
+  launchCamera = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchCamera(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+  }
+
+  launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
   }
 }
 
@@ -38,5 +158,5 @@ const styles = StyleSheet.create({
   logo: {
     height: 50,
     width: 50,
-  },
+  }
 });
