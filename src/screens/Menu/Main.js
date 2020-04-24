f33fa98741f5130e6848ddc38870153e4dc25430
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2020-04-24 18:23:58 +0800

    jr
    jr

--- src/screens/Menu/Main.js
+++ src/screens/Menu/Main.js
@@ -9,14 +9,44 @@ import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Content from './Content';
 
+
+import { Avatar, Badge, withBadge } from 'react-native-elements'
+
 const deviceWidth = Dimensions.get('window').width;
 
 export default function Main ({route,navigation,body_content}) {
   const [menu_list, setMenu_list] = React.useState(null);
   var [content, setcontent] = React.useState(null);
-  
+  var [badge_val, setbadge_val] = React.useState(null);
+  var [badge_hidden, setbadge_hidden] = React.useState(false);
   useFocusEffect(
     React.useCallback(() => {
+
+//check cart
+AsyncStorage.getAllKeys((err, keys) => {
+      AsyncStorage.multiGet(keys, (err, stores) => {
+        const newData= stores.map((result, i, store) => {
+            let key = store[i][0];
+            var jsonPars = JSON.parse(store[i][1]);
+            if(jsonPars.add_to_cart==1){
+              // setItemStorage(n.toString(),{'id':n.toString(),'selected_additions':0,'add_to_cart':1,'product_id':product_id,'product_name': product_name,'qty':current_qty,'price':current_price})
+                return jsonPars;
+            }
+          });
+          try{
+            var filtered_newData = newData.filter(e => e != null);
+       
+            if(filtered_newData.length==0){
+              setbadge_hidden(true);
+            }else{
+              
+              setbadge_val(filtered_newData.length);
+              setbadge_hidden(false);
+            }
+          }catch(error){}
+        });
+});
+
       setcontent(<Content category_id={0}/>);
       fetch(global.global_url+'menu/get_all_tags.php')
             .then((response) => response.json())
@@ -43,6 +73,7 @@ export default function Main ({route,navigation,body_content}) {
     <View style={{  flexDirection: 'row', padding:2,}} >
         <TouchableOpacity onPress={() =>   navigation.navigate('Cart')}>
         <Icon name="cart-plus" size={25} color={"#ffff"} style={{paddingLeft:10,paddingTop:10,paddingRight:10}}/>
+          {badge_hidden ? null : (<Badge value={badge_val} status="error" containerStyle={styles.badgeStyle}/>)}
         </TouchableOpacity>
         <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}>
         <Icon name="comments" size={25} color={"#ffff"} style={{paddingLeft:10,paddingTop:10,paddingRight:10}}/>
@@ -177,5 +208,10 @@ const styles = StyleSheet.create({
         color:'#4A4A4A',
         padding:10,
         fontSize: 18,
+      },
+      badgeStyle: { 
+        position: 'absolute',
+        top: 0,
+        right: -4 
       },
 })
